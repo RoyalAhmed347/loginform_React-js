ecb4cd90df6af0fe4d23532af64fa7a8ae258ca9
Royal Ahmed <bahiahmed350@gmail.com>
2023-10-12 11:23:14 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import './App.css'
+const App = () => {
 
-function App() {
+  const [data, setData] = useState({
+    name: "",
+    password: "",
+    email: "",
+    phone: ""
+  });
+
+  const inputEvent = (e) => {
+    const {name, value} = e.target
+  
+
+    setData((preData) =>{
+      return{
+        ...preData,
+        [name]: value
+      }
+    })
+  }
+  const submit = (e) => {
+    e.preventDefault();
+
+    if (data.name === "") {
+      alert("Name can't empty")
+    } else if(data.password === ""){
+      alert("Password can't empty")
+    }
+     else if(data.email === ""){
+      alert("Email can't empty")
+    }
+     else if(data.phone === ""){
+      alert("Phone Number can't empty")
+    } else{
+      alert('Submited')
+    }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <div className='main'>
+        <form onSubmit={submit}>
+          <h1>Hello {data.name}</h1>
+          <p>{data.email}</p>
+          <p>{data.phone}</p>
+          <input autoComplete='off' value={data.name} name='name' onChange={inputEvent} type='text' placeholder='Enter Your First Name'/>
+          <input autoComplete='off' value={data.password} name='password' onChange={inputEvent} type='password' placeholder='Enter Your Password'/>
+          <input autoComplete='off' value={data.email} name='email' onChange={inputEvent} type='email' placeholder='Enter Your Email'/>
+          <input autoComplete='off' value={data.phone} name='phone' onChange={inputEvent} type='number' placeholder='Enter Your Phone Number'/>
+          <button type='submit'>Submit</button>
+        </form>
+      </div>
+    </>
+  )
 }
 
-export default App;
+export default App
